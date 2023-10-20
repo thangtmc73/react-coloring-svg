28cc8a7e55c1427fd4a45d43cfca759a64194aac
thangtm2 <thangtm2@vng.com.vn>
2023-10-20 17:22:18 +0700

    feat: handle click item with color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react"
 import './App.css'
-
+import ColorContext from "./colorContext";
+import SvgMonaLisa from "./svg-components/MonaLisa";
 function App() {
+  const [colors, setColors] = useState<MapColor>({});
+  const [selectedColor, setSelectedColor] = useState("#ffff");
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <div style={{ height: '100vh', width: '100%' }}>
+      <ColorContext.Provider value={colors}>
+        <SvgMonaLisa
+          colors={colors}
+          onItemClick={(name: string) => {
+            console.log("coloring", name, selectedColor);
+          }}
+          style={{ width: '256px', height: '256px' }} />
+      </ColorContext.Provider>
+    </div>
   )
 }
 
---
+++ src/colorContext.tsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const ColorContext = createContext<MapColor>({});
+
+export default ColorContext;
